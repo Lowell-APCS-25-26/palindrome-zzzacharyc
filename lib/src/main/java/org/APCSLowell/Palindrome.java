77ec140807ddbae8141c74ed92e3399f9dfc0540
Zachary <zachary.choi@icloud.com>
2026-02-02 18:04:11 +0000

    I finished it

--- lib/src/main/java/org/APCSLowell/Palindrome.java
+++ lib/src/main/java/org/APCSLowell/Palindrome.java
@@ -2,11 +2,21 @@ package org.APCSLowell;
 
 public class Palindrome {
     public boolean palindrome(String s) {
-        return false;
+         String Z = "";
+           for (int i = 0; i < s.length(); i++)
+                if (!s.substring(i, i + 1).equals(" "))
+                    if(Character.isLetter(s.charAt(i)))
+                    Z = Z + (s.substring(i, i + 1));
+        Z = Z.toLowerCase();
+        if (Z.equals(reverse(Z)))
+            return true;
+                return false;
     }
     public String reverse(String s) {
-        String n = new String();
-        // your code here
-        return n;
+        String N = "";
+           for (int i = s.length()-1; i >= 0; i--)
+                if (!s.substring(i, i + 1).equals(" "))
+                    N = N + (s.substring(i, i + 1));
+        return N.toLowerCase();
     }
 }
